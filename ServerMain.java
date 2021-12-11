856bba6102e492572379e5575744d3485625a581
robinp12 <robin-be@hotmail.com>
2021-12-11 23:00:47 +0100

    one server, multiple client DONE

--- ServerMain.java
+++ ServerMain.java
@@ -12,9 +12,6 @@ import java.security.spec.InvalidKeySpecException;
 
 public class ServerMain{
 
-    private static String pwd;
-    private static boolean isFound = false;
-
     /**
      * @param in Stream from which to read the request
      * @return Request with information required by the server to process encrypted file
@@ -41,6 +38,104 @@ public class ServerMain{
         return generatedPassword;
     }
 
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException,
+            InvalidKeySpecException, NoSuchPaddingException,
+            IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InterruptedException {
+
+        // Template decrypted file
+        File decryptedFile = new File("test_file-decrypted-server.pdf");
+        // Template file from client
+        File networkFile = new File("temp-server.pdf");
+
+        ServerSocket ss = new ServerSocket(3333);
+        System.out.println("Waiting connection");
+
+        while (true) {
+            // Create socket
+            Socket socket = ss.accept();
+            System.out.println("Connection from: " + socket);
+            // Stream to read request from socket
+            InputStream inputStream = socket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            // Stream to write response to socket
+            DataOutputStream outSocket = new DataOutputStream(socket.getOutputStream());
+            // Stream to write the file to decrypt
+            OutputStream outFile = new FileOutputStream(networkFile);
+
+            // Read datas from client
+            Request request = readRequest(dataInputStream);
+            long fileLength = request.getLengthFile();
+            int pwdLength = request.getLengthPwd();
+            String hashPwd = arrayHashToString(request.getHashPassword());
+            System.out.println("fileLength: " + fileLength);
+            System.out.println("pwdLength: " + pwdLength);
+            System.out.println("hashPwd: " + hashPwd);
+
+            // GET THE RESPONSE FROM THE CLIENT
+            FileManagement.receiveFile(inputStream, outFile, fileLength);
+            /*
+    int readFromFile = 0;
+    int bytesRead = 0;
+    byte[] readBuffer = new byte[64];
+    System.out.println("[Server] File length: "+ fileLength);
+    while((readFromFile < fileLength)){
+        bytesRead = inputStream.read(readBuffer);
+        readFromFile += bytesRead;
+        outFile.write(readBuffer, 0, bytesRead);
+    }*/
+
+            /* Bruteforce password with thread */
+            BruteForcing bruteForcing = new BruteForcing(pwdLength, hashPwd);
+
+            Thread thread = new Thread(bruteForcing);
+            // Running thread
+            thread.start();
+            // Waiting thread to stop processing before continuing instructions
+            thread.join();
+
+            SecretKey serverKey = CryptoUtils.getKeyFromPassword(BruteForcing.getPwd());
+
+            CryptoUtils.decryptFile(serverKey, networkFile, decryptedFile);
+
+            // Send the decryptedFile
+            InputStream inDecrypted = new FileInputStream(decryptedFile);
+            outSocket.writeLong(decryptedFile.length());
+            outSocket.flush();
+            FileManagement.sendFile(inDecrypted, outSocket);
+            /*
+    int readCount;
+    byte[] buffer = new byte[64];
+    //read from the file and send it in the socket
+    while ((readCount = inDecrypted.read(buffer)) > 0){
+        outSocket.write(buffer, 0, readCount);
+    }*/
+
+            dataInputStream.close();
+            inputStream.close();
+            inDecrypted.close();
+            outFile.close();
+            socket.close();
+        }
+    }
+}
+
+class BruteForcing implements Runnable{
+
+    private final int pwdLength;
+    private final String hashPwd;
+
+    private static String pwd = "";
+    private static boolean isFound = false;
+
+    public BruteForcing(int pwdLength, String hashPwd){
+        this.pwdLength = pwdLength;
+        this.hashPwd = hashPwd;
+    }
+
+    public static String getPwd() {
+        return pwd;
+    }
+
     private static String passwordToHash(String passwordToHash) {
         String generatedPassword = null;
         try {
@@ -57,7 +152,7 @@ public class ServerMain{
         return generatedPassword;
     }
 
-    protected static String bruteForceDictionnary(String word) throws IOException {
+    private static String bruteForceDictionnary(String word) throws IOException {
         String st;
         File dictionnary = new File("10k-most-common_filered.txt");
         BufferedReader br = new BufferedReader(new FileReader(dictionnary));
@@ -70,7 +165,7 @@ public class ServerMain{
         return null;
     }
 
-    protected static void bruteForceAlphabet(String prefix, int mdpLength, String hash) {
+    private static void bruteForceAlphabet(String prefix, int mdpLength, String hash) {
         for (char i = 'a'; i <= 'z'; i++) {
             if (mdpLength == 0) {
                 System.out.println(prefix);
@@ -86,91 +181,6 @@ public class ServerMain{
         }
     }
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException,
-            InvalidKeySpecException, NoSuchPaddingException,
-            IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InterruptedException {
-        // Template decrypted file
-        File decryptedFile = new File("test_file-decrypted-server.pdf");
-        // Template file from client
-        File networkFile = new File("temp-server.pdf");
-
-        ServerSocket ss = new ServerSocket(3333);
-        System.out.println("Waiting connection");
-
-        // Create socket
-        Socket socket = ss.accept();
-        System.out.println("Connection from: " + socket);
-        // Stream to read request from socket
-        InputStream inputStream = socket.getInputStream();
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-        // Stream to write response to socket
-        DataOutputStream outSocket = new DataOutputStream(socket.getOutputStream());
-        // Stream to write the file to decrypt
-        OutputStream outFile = new FileOutputStream(networkFile);
-
-        // Read datas from client
-        Request request = readRequest(dataInputStream);
-        long fileLength = request.getLengthFile();
-        int pwdLength = request.getLengthPwd();
-        String hashPwd = arrayHashToString(request.getHashPassword());
-
-        // GET THE RESPONSE FROM THE CLIENT
-        FileManagement.receiveFile(inputStream, outFile, fileLength);
-        /*
-        int readFromFile = 0;
-        int bytesRead = 0;
-        byte[] readBuffer = new byte[64];
-        System.out.println("[Server] File length: "+ fileLength);
-        while((readFromFile < fileLength)){
-            bytesRead = inputStream.read(readBuffer);
-            readFromFile += bytesRead;
-            outFile.write(readBuffer, 0, bytesRead);
-        }*/
-
-        /* Bruteforce password with thread */
-        BruteForcing bruteForcing = new BruteForcing(pwdLength, hashPwd);
-
-        Thread thread = new Thread(bruteForcing);
-        // Running thread
-        thread.start();
-        // Waiting thread to stop processing before continuing instructions
-        thread.join();
-
-        SecretKey serverKey = CryptoUtils.getKeyFromPassword(pwd);
-
-        CryptoUtils.decryptFile(serverKey, networkFile, decryptedFile);
-
-        // Send the decryptedFile
-        InputStream inDecrypted = new FileInputStream(decryptedFile);
-        outSocket.writeLong(decryptedFile.length());
-        outSocket.flush();
-        FileManagement.sendFile(inDecrypted, outSocket);
-        /*
-        int readCount;
-        byte[] buffer = new byte[64];
-        //read from the file and send it in the socket
-        while ((readCount = inDecrypted.read(buffer)) > 0){
-            outSocket.write(buffer, 0, readCount);
-        }*/
-
-        dataInputStream.close();
-        inputStream.close();
-        inDecrypted.close();
-        outFile.close();
-        socket.close();
-    }
-}
-
-class BruteForcing implements Runnable{
-
-    private final int pwdLength;
-    private final String hashPwd;
-
-    public BruteForcing(int pwdLength, String hashPwd){
-        this.pwdLength = pwdLength;
-        this.hashPwd = hashPwd;
-    }
-
     /**
      * When an object implementing interface <code>Runnable</code> is used
      * to create a thread, starting the thread causes the object's
@@ -184,6 +194,7 @@ class BruteForcing implements Runnable{
      */
     @Override
     public void run() {
-        ServerMain.bruteForceAlphabet("", pwdLength,hashPwd);
+        isFound = false;
+        bruteForceAlphabet("", pwdLength,hashPwd);
     }
 }
