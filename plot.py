4562562a86afca0dedb0c8e8ea681cc8533a9356
Luc <luc.gabrys@student.uclouvain.be>
2021-12-28 15:20:25 +0100

    Graph 3

--- plot.py
+++ plot.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import numpy.polynomial.polynomial as poly
 from scipy.interpolate import CubicSpline as CS
 
-files = ["printfile.txt", "printfile_wait.txt", ]
+files = ["printfile.txt", "printfile_wait.txt", "printfile_pass.txt", "printfile_pass_opti.txt"]
 titles = ["Server response time"]
 x_axis = 'Number of clients'
+X_axis2 = 'Password length'
 y_axis = 'Execution time (ms)'
-names = ["server"]
+names = ["server", "server-client-delay", "password"]
 
 
 def get_data(dictionary, file):
@@ -22,7 +23,8 @@ def get_data(dictionary, file):
         password_length = int(pl)
 
         l = dictionary.get(thread_number, [])
-        l.append((thread_number, file_length, time, password_length))
+        l.append((file_length, time, password_length))
+        # print(l)
         dictionary[thread_number] = l
 
 d1 = {}
@@ -34,11 +36,14 @@ x3 = linspace(1, 100, 10000)
 get_data(d1, files[0])
 get_data(d2, files[1])
 get_data(d3, files[2])
-get_data(d4, files[4])
+get_data(d4, files[3])
 
 
 x = list(d1.keys())
-y = [sum(v) / len(v) for v in d1.values()]
+print(d1.values())
+for x in d1.values():
+    print(x)
+y = [sum(tim) / len(tim) for (fs, tim, pl) in [c for c in d1.values()]]
 plt.stem(x, y, label='Clients sending requests at the same time')
 
 
@@ -48,4 +53,32 @@ plt.ylim(bottom=0)
 plt.grid()
 # plt.legend(loc='best', ncol=2, fontsize='xx-small', labelspacing=0)
 plt.savefig(names[0] + '.png', bbox_inches='tight')
+plt.show()
+
+x = list(d2.keys())
+y = [sum(tim) / len(tim) for (fs, tim, pl) in d2.values()]
+plt.stem(x, y, label='Clients requests delayed')
+
+
+plt.title(titles[0])
+plt.ylabel(y_axis)
+plt.ylim(bottom=0)
+plt.grid()
+# plt.legend(loc='best', ncol=2, fontsize='xx-small', labelspacing=0)
+plt.savefig(names[1] + '.png', bbox_inches='tight')
+plt.show()
+
+y = [pl for (fs, tim, pl) in d3.values()]
+y = [sum(tim) / len(tim) for (fs, tim, pl) in d3.values()]
+plt.plot(x, y, label='Basic server')
+y = [pl for (fs, tim, pl) in d4.values()]
+y = [sum(tim) / len(tim) for (fs, tim, pl) in d4.values()]
+plt.plot(x, y, label='Optimized server')
+
+plt.title(titles[0])
+plt.ylabel(y_axis)
+plt.ylim(bottom=0)
+plt.grid()
+# plt.legend(loc='best', ncol=2, fontsize='xx-small', labelspacing=0)
+plt.savefig(names[2] + '.png', bbox_inches='tight')
 plt.show()
